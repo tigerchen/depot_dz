3ca74ef4920c85c8783aa919a2b05ef9de1b8426
Dazhi Chen <dazhichen@gmail.com>
2011-10-30 13:17:16 -0400

    more validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :title, length: { :minimum => 10, :message => "must be 10 characters minimum" }
   validates :image_url, allow_blank: true, format:
   {
     with: %r(\.(gif|jpg|png)$)i,
