90accb67bbf89278f10394a4b5e0db138929ac1c
Dazhi Chen <dazhichen@gmail.com>
2011-10-30 22:16:56 -0400

    Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,18 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    
+    if session[:counter].nil?
+      session[:counter]=1
+    else
+      session[:counter] += 1
+    end
+    
+    if session[:counter] > 5
+      @session_counter = session[:counter]
+    else
+      @session_counter = nil
+    end 
   end
 
 end
