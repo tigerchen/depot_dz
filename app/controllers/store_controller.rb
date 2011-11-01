4b71b615c5904b75032b4a4127979e1d3131e92a
dazhi chen <dazhichen@gmail.com>
2011-11-01 17:49:58 -0400

    chapter 11

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,17 +3,6 @@ class StoreController < ApplicationController
     @products = Product.order(:title)
     @cart = current_cart
     
-    if session[:counter].nil?
-      session[:counter]=1
-    else
-      session[:counter] += 1
-    end
-    
-    if session[:counter] > 5
-      @session_counter = session[:counter]
-    else
-      @session_counter = nil
-    end 
   end
 
 end
